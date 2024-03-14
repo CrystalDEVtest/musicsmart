6727af9d1d6d59ab5faa16b1ab1b57292e6416ed
CrystalDEVtest <161251881+CrystalDEVtest@users.noreply.github.com>
2024-03-14 19:16:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require 'vendor/autoload.php';
 use VK\Client\VK as VK;
 
 // Ваш токен от ВКонтакте
-$token = 'ваш_токен';
+$token = 'vk1.a.wx4tqcdYoN7JQGGUUU3UDOuupDw_9KCpVKqWpz-4PKum5SCDQxwcnD1HbeC8N2mysGmDkDE1eipx_VbZBt0qQIxVZW3IOxo2qB-tf5hcPe6Bdl_CNd-0dUl0YIyhO_LLgTSlv7bhCtfyGHnuYaI4zkDC4FN-VbhixFrY-5trI_Sgys-ABG9T6Adj-4LyUL6EE0K1Sflx0kreyHzubSAUcQ&expires_in=86400&user_id=468050309';
 
 // Инициализация клиента
 $vk = new VK($token);
@@ -27,5 +27,7 @@ echo $audio_url['url'];
     <p>Имя: <input type="text" name="audio_url" /></p>
     <input type="submit" value="Отправить">
 </form>
+<form method="POST">
+    <p>Вот ссылка: <input="text" name="audip_url['url']" /></p>
 </body>
 </html>
